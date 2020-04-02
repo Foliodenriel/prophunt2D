0c4800671149f9b6802d7c5d9bea572179bce660
BaraAlexi <alexibara.dev@gmail.com>
2020-04-02 22:20:12 +0200

    [add] assets / sync movement (glitchy)

--- js/game.js
+++ js/game.js
@@ -3,6 +3,13 @@ var config = {
     width: 800,
     height: 600,
     parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 680 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -12,16 +19,119 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var platforms;
+var player;
+var otherPlayers;
+var cursors;
+var client;
+
 function preload() {
 
-    this.client = new Client(); // Generating client class for socket handling
+    client = new Client(); // Generating client class for socket handling
+
+    //this.load.setBaseURL('http://127.0.0.1:3000/');
+
+    this.load.image('player', 'assets/gobble.png');
+    this.load.image('ground', 'assets/ground.png');
 }
 
 function create() {
 
-    this.client.addNewPlayer(); // Emitting signal to server to get response signal for every socket instance
+    var self = this;
+
+    player = this.physics.add.image(200, 200, 'player').setScale(0.1);
+    player.setBounce(0);
+    player.setCollideWorldBounds(true);
+    player.oldPos = {
+        posX: player.x,
+        posY: player.y,
+    }
+
+    platforms = this.physics.add.staticGroup();
+    otherPlayers = this.physics.add.group();
+
+    client.socket.on('currentPlayers', function(players) {
+
+        Object.keys(players).forEach(function(id) {
+            if (players[id].playerId !== client.socket.id) {
+
+                addPlayer(self, players[id]);
+            }
+        })
+    });
+
+    client.socket.on('newPlayer', function(player) {
+
+        const otherPlayer = self.physics.add.image(200, 200, 'player').setScale(0.1);
+        otherPlayer.playerId = player.playerId;
+        otherPlayers.add(otherPlayer);
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(otherPlayers, platforms);
+
+    platforms.create(200, 400, 'ground').setScale(0.1).refreshBody();
+    platforms.create(230, 400, 'ground').setScale(0.1).refreshBody();
+    platforms.create(260, 400, 'ground').setScale(0.1).refreshBody();
+    platforms.create(290, 400, 'ground').setScale(0.1).refreshBody();
+    platforms.create(320, 400, 'ground').setScale(0.1).refreshBody();
+
+    platforms.create(380,340, 'ground').setScale(0.1).refreshBody();
+
+    client.addNewPlayer(); // Emitting signal to server to get response signal for every socket instance
 }
 
 function update() {
 
+    if (cursors.left.isDown) {
+
+        player.setVelocityX(-200);
+    } else if (cursors.right.isDown) {
+
+        player.setVelocityX(200);
+    } else if (cursors.left.isUp) {
+
+        player.setVelocityX(0);
+    } else if (cursors.right.isUp) {
+
+        player.setVelocityX(0);
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-300);
+    }
+
+    if (player.oldPos.posX !== player.posX || player.oldPos.posY !== player.posY) {
+
+        var newPlayerObjectPos = {
+            posX: player.x,
+            posY: player.y,
+            playerId: client.socket.id
+        }
+        //console.log(newPlayerObjectPos);
+        client.socket.emit('movePlayer', newPlayerObjectPos);
+    }
+
+    client.socket.on('newPlayerPos', function(movingPlayer) {
+
+        otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (otherPlayer.playerId === movingPlayer.playerId) {
+                otherPlayer.setPosition(movingPlayer.posX, movingPlayer.posY);
+            }
+        });
+    });
+
+    player.oldPos = {
+        posX: player.x,
+        posY: player.y,
+    }
+}
+
+function addPlayer(self, player) {
+
+    // console.log('(' + player.posX + ', ' + player.posY + ')');
+    self.physics.add.image(player.posX, player.posY, 'player').setOrigin(0.5, 0.5).setScale(0.1);
 }
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var players = {};
+
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
@@ -18,19 +20,36 @@ http.listen(3000, function() {
 
 io.on('connection', function(socket) {
 
-    console.log('Welcome message!');
+    players[socket.id] = {
 
-    socket.on('addnewplayer', function() {
+        posX: Math.floor(Math.random() * 700) + 50,
+        posY: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+    };
 
-        // Do things you do when adding new player such as filling socket
+    console.log('Welcome message! player in this room: ' + Object.keys(players).length);
+
+    socket.emit('currentPlayers', players);
+    console.log('currentPlayers emitted.');
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('movePlayer', function(player) {
+
+        players[player.playerId].posX = player.posX;
+        players[player.playerId].posY = player.posX;
+        socket.broadcast.emit('newPlayerPos', players[player.playerId]);
+        socket.emit('wow', 'This is a message from server!');
     });
 
     // Send emit signal for current socket info to get back
     // Send broadcast signal for all other users to get info about your connection
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function(socket) {
 
         console.log('Good bye!');
+
+        delete players[socket.id];
+        io.emit('disconnect', socket.id);
     });
 
 });
